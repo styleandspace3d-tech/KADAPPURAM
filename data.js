00d7b2c7c7fb0f090f8c19bd114c5015b6eeab69
styleandspace3d-tech <styleandspace3d@gmail.com>
2026-09-20 22:47:18 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,341 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ground-floor-living-room",
+      "name": "GROUND FLOOR LIVING ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-ground-floor-living-room"
+        },
+        {
+          "yaw": 0.6219825734252762,
+          "pitch": 0.2014308659322559,
+          "rotation": 0,
+          "target": "1-dining-room"
+        },
+        {
+          "yaw": 0.6132956420856175,
+          "pitch": -0.05074120921776881,
+          "rotation": 7.853981633974483,
+          "target": "2-prayer-room"
+        },
+        {
+          "yaw": 0.5046399623810416,
+          "pitch": 0.07614600442054886,
+          "rotation": 5.497787143782138,
+          "target": "3-ladies-dining"
+        },
+        {
+          "yaw": 0.6862116043214126,
+          "pitch": -1.0202111063400743,
+          "rotation": 0,
+          "target": "4-stair-area"
+        },
+        {
+          "yaw": 0.4269909936741758,
+          "pitch": -0.8002252697399541,
+          "rotation": 0.7853981633974483,
+          "target": "5-first-floor-formal-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dining-room",
+      "name": "DINING ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05342844648962952,
+          "pitch": 0.3930750772999314,
+          "rotation": 0,
+          "target": "1-dining-room"
+        },
+        {
+          "yaw": 0.43095640080872144,
+          "pitch": 0.04372724224078084,
+          "rotation": 0,
+          "target": "2-prayer-room"
+        },
+        {
+          "yaw": 2.257693940044259,
+          "pitch": 0.1284106877793647,
+          "rotation": 0,
+          "target": "0-ground-floor-living-room"
+        },
+        {
+          "yaw": -1.2076366715577933,
+          "pitch": 0.11734511823917515,
+          "rotation": 0,
+          "target": "3-ladies-dining"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-prayer-room",
+      "name": "PRAYER ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.6143900469960002,
+        "pitch": 0.01752114493533874,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6553518559713769,
+          "pitch": 0.05006041410095108,
+          "rotation": 0,
+          "target": "2-prayer-room"
+        },
+        {
+          "yaw": -2.834408746742028,
+          "pitch": 0.29170854104147104,
+          "rotation": 0,
+          "target": "1-dining-room"
+        },
+        {
+          "yaw": -2.5772762076186506,
+          "pitch": 0.04369482071291131,
+          "rotation": 0.7853981633974483,
+          "target": "3-ladies-dining"
+        },
+        {
+          "yaw": 3.0118860973825266,
+          "pitch": 0.11205805808162239,
+          "rotation": 5.497787143782138,
+          "target": "0-ground-floor-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ladies-dining",
+      "name": "LADIES DINING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "3-ladies-dining"
+        },
+        {
+          "yaw": -3.0241067080586443,
+          "pitch": 0.23184796607996816,
+          "rotation": 10.995574287564278,
+          "target": "1-dining-room"
+        },
+        {
+          "yaw": -2.8726858036947114,
+          "pitch": 0.04596090285858523,
+          "rotation": 0,
+          "target": "0-ground-floor-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-stair-area",
+      "name": "STAIR AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0587712911385907,
+        "pitch": 0.40799237492265483,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0587712911385907,
+          "pitch": 0.40799237492265483,
+          "rotation": 0,
+          "target": "4-stair-area"
+        },
+        {
+          "yaw": -0.1445396568503714,
+          "pitch": 0.7189888463027891,
+          "rotation": 3.141592653589793,
+          "target": "0-ground-floor-living-room"
+        },
+        {
+          "yaw": 2.011980568516435,
+          "pitch": 0.20673299835611303,
+          "rotation": 0,
+          "target": "5-first-floor-formal-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-first-floor-formal-living",
+      "name": "FIRST FLOOR FORMAL LIVING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "5-first-floor-formal-living"
+        },
+        {
+          "yaw": 2.5594204699880585,
+          "pitch": 0.10818611714037019,
+          "rotation": 0,
+          "target": "4-stair-area"
+        },
+        {
+          "yaw": 3.048390348180966,
+          "pitch": 0.2615903139456002,
+          "rotation": 3.141592653589793,
+          "target": "0-ground-floor-living-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "KADAPPURAM ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
